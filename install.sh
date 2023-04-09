501e8f404cce1653bbf7400e4988a5bf81bf739c
chitacan <chitacan@gmail.com>
2023-04-10 00:01:13 +0900

    add .default_files

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/bin/bash
 
 echo "install dotfiles..."
 
@@ -6,5 +6,20 @@ cp .gitconfig ~/
 cp .gitignore ~/
 cp .tigrc ~/
 cp .iex.exs ~/
+
+if [[ -d ~/.config/fish ]]; then
+  mkdir -p ~/.config/fish
+fi
+
 cp .config/fish/config.fish ~/.config/fish/
 cp .config/fish/fish_plugins ~/.config/fish/
+
+# setup default_files
+if [[ -n "$CODESPACES" && -n "$GITHUB_REPOSITORY" && -d ".default_files/$GITHUB_REPOSITORY" ]]; then
+  if [[ ! -d /workspaces/.default_files ]]; then
+    mkdir /workspaces/.default_files
+  fi
+
+  cp -an .default_files/$GITHUB_REPOSITORY/. /workspaces/.default_files/
+  cp -an .default_files/$GITHUB_REPOSITORY/. /workspaces/$RepositoryName
+fi
