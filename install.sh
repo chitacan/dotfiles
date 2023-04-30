277bee2c9dce3734350b65d80da181a99942516c
Kyungyeol Kim <chitacan@gmail.com>
2023-04-30 19:07:59 +0900

    setup nvim

--- install.sh
+++ install.sh
@@ -14,6 +14,13 @@ if [[ ! -d "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim ]]
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 fi
 
+# setup nvim
+if [[ ! -d ~/.config/nvim ]]; then
+  mkdir -p ~/.config/nvim
+fi
+
+cp .config/nvim/init.vim ~/.config/nvim/
+
 # setup fish config
 if [[ ! -d ~/.config/fish ]]; then
   mkdir -p ~/.config/fish
