6e2837e72752d269d4e4e0f55318dafc6085edff
Kyungyeol Kim <chitacan@gmail.com>
2023-06-30 15:58:31 +0900

    start tracer when run api server

--- .default_files/corp-momenti/momenti-elixir/apps/momenti_api/.iex.exs
+++ .default_files/corp-momenti/momenti-elixir/apps/momenti_api/.iex.exs
@@ -4,3 +4,22 @@ use MomentiDomain
 use MomentiDomain.V4.Studio
 use MomentiDomain.V4.Content
 use MomentiDomain.V4.Accounts
+
+:dbg.start()
+
+:dbg.tracer(:process, {
+  fn
+    {:trace, _pid, :call,
+     {Studio.ContentLogic, :update_file_info_of_moment_model_info,
+      [_project_id, file_info, _env]}, _parrent},
+    _ ->
+      T.decode_giv(file_info)
+
+    msg, _ ->
+      IO.inspect(msg, label: "Trace not matched")
+  end,
+  0
+})
+
+:dbg.tpl(Studio.ContentLogic, :update_file_info_of_moment_model_info, 3, :c)
+:dbg.p(:all, :c)
