f0348b234d96a38ebbd8bda6a5d51fdffc507633
Kyungyeol Kim <chitacan@gmail.com>
2023-07-03 16:21:35 +0900

    handle error for decode_giv

--- .iex.exs
+++ .iex.exs
@@ -193,6 +193,9 @@ defmodule Tool do
            decoded <- apply(module, :decode, [body]) |> inspect(pretty: true, limit: :infinity),
            :ok <- File.write(target_path, decoded) do
         target_path
+      rescue
+        error ->
+          error
       end
     end
 
